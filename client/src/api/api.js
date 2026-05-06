bfb46168d30a83882b9cf67dbbcb91a20f7baa6e
Gaurav Kumar <gauravkreth@gmail.com>
2026-05-06 21:51:23 +0530

    fixed for deployment

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-resume-platform-qwc6.onrender.com/api",
 });
 
 export default API;
